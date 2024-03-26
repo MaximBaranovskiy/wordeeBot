af012a4d159c05f57b11a0f68b6883a9d772ce67
maxim <maximbaranovskiy26042005@gmail.com>
2024-03-26 19:16:16 +0300

    refactor environment

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"log"
+	"os"
 	"wordeeBot/internal/bot"
 )
 
 func main() {
-	bot, err := bot.NewBot("BOT_TOKEN")
+	bot, err := bot.NewBot(os.Getenv("BOT_TOKEN"))
 	if err != nil {
 		log.Fatal(err)
 	}
