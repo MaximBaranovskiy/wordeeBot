0e9e444b776e442438a6070bc31e3d9aadc5bf56
maxim <maximbaranovskiy26042005@gmail.com>
2024-03-26 19:14:52 +0300

    refactor handlers.go

--- internal/bot/handlers.go
+++ internal/bot/handlers.go
@@ -6,49 +6,33 @@ import (
 	"wordeeBot/internal/myErrors"
 )
 
+type handlerFunc func(b *TgBotModel, update tgbotapi.Update) error
+
+var callbackData2Handler = map[string]handlerFunc{
+	"myDictionaries":   handleShowDictionaries,
+	"createDictionary": handleCreateDictionary,
+	"editDictionary":   handleEditDictionary,
+	"mainMenu":         handleMainMenu,
+	"addingWord":       handleAddingWord,
+	"cancelWord":       handleCancelledAddingWord,
+}
+
+var usrLstCommand2Handler = map[string]handlerFunc{
+	"create_dictionary_name": handleCreateDictionaryColumns,
+	"edit_dictionary":        handleChooseDictionaryForEdit,
+	"editDictionary_name":    handleDictionaryEditing,
+	"myDictionaries":         handleSendParticularDictionary,
+}
+
 func handleCallbacks(b *TgBotModel, update tgbotapi.Update) {
-	switch {
+	if handler, ok := callbackData2Handler[update.CallbackData()]; ok {
+		myErrors.HandleError(b.bot, update.CallbackQuery.Message.Chat.ID, handler(b, update))
+		return
+	}
 
-	case strings.Contains(update.CallbackQuery.Data, "myDictionaries"):
-
-		myErrors.HandleError(b.bot, update.CallbackQuery.Message.Chat.ID, handleShowDictionaries(b, update))
-
-	case strings.Contains(update.CallbackQuery.Data, "createDictionary"):
-
-		myErrors.HandleError(b.bot, update.CallbackQuery.Message.Chat.ID, handleCreateDictionary(b, update))
-
-	case strings.Contains(update.CallbackQuery.Data, "editDictionary"):
-
-		myErrors.HandleError(b.bot, update.CallbackQuery.Message.Chat.ID, handleEditDictionary(b, update))
-
-	case strings.Contains(update.CallbackQuery.Data, "mainMenu"):
-
-		myErrors.HandleError(b.bot, update.CallbackQuery.Message.Chat.ID, handleMainMenu(b, update))
-
-	case b.userLastCommand[update.CallbackQuery.From.ID] == "createDictionary_name":
-
-		myErrors.HandleError(b.bot, update.CallbackQuery.Message.Chat.ID, handleCreateDictionaryColumns(b, update))
-
-	case b.userLastCommand[update.CallbackQuery.From.ID] == "editDictionary":
-
-		myErrors.HandleError(b.bot, update.CallbackQuery.Message.Chat.ID, handleChooseDictionaryForEdit(b, update))
-
-	case b.userLastCommand[update.CallbackQuery.From.ID] == "editDictionary_name":
-
-		myErrors.HandleError(b.bot, update.CallbackQuery.Message.Chat.ID, handleDictionaryEditing(b, update))
-
-	case strings.Contains(update.CallbackQuery.Data, "addingWord"):
-
-		myErrors.HandleError(b.bot, update.CallbackQuery.Message.Chat.ID, handleAddingWord(b, update))
-
-	case strings.Contains(update.CallbackQuery.Data, "cancelWord"):
-
-		myErrors.HandleError(b.bot, update.CallbackQuery.Message.Chat.ID, handleCancelledAddingWord(b, update))
-
-	case b.userLastCommand[update.CallbackQuery.From.ID] == "myDictionaries":
-
-		myErrors.HandleError(b.bot, update.CallbackQuery.Message.Chat.ID, handleSendParticularDictionary(b, update))
-
+	if handler, ok := usrLstCommand2Handler[b.userLastCommand[update.CallbackQuery.From.ID]]; ok {
+		myErrors.HandleError(b.bot, update.CallbackQuery.Message.Chat.ID, handler(b, update))
+		return
 	}
 }
 
